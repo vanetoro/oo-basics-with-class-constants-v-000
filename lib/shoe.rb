8e7ccf22927ab4f94f86f9771ea4005e8b3a4c3f
Vanessa Toro <vanetoro84@gmail.com>
2018-03-27 17:59:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do | shoe_brand |
-      if @brand != shoe_brand
-        BRANDS << @brand
+    !BRAND.include?(brand)
       end  
     end
   end
