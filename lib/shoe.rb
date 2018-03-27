86da2a97123900650b2d7b20221da3fc4cec8fed
Vanessa Toro <vanetoro84@gmail.com>
2018-03-27 17:21:46 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,8 +8,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do | show_brand |
-      if @brand != show_brand
-        BRANDS << @brand
+      if brand != show_brand
+        BRANDS << brand
       end  
     end
   end
